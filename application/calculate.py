61fa26cfd8e1cb37290e4aa84cf2e0b6a9bec44c
Collins Pamba <collins@buffalo.co.ke>
2021-07-29 15:45:24 +0300

    added solution to closest points pair problem, added unit tests for calculate functions

--- application/calculate.py
+++ application/calculate.py
@@ -1,10 +1,73 @@
 """calculate.py
 Module where closest points calculation is performed
 """
-def closest_pair(points_string):
-    """Calculate the closest pair"""
-    # get points in a list
-    points_list = list(zip(points_string))
+import math
 
-    # postpone closest pair calculation for the time being
-    return points_list[0:2]
+
+def closest_pair(points_string):
+    """Calculate the closest pair.
+    points_string is a validated string containing tuples e.g. (2,3),(1.8,99),...
+    """
+    # get points in a list
+    points_list = generate_tuples_list_from_string(points_string)
+
+    # here we have a list containing tuples like (1,2),(3,4)
+    # go through each tuple and calculate the distance with other tuples in the list
+    distances = []
+    for p in points_list:
+        for q in points_list:
+            # skip if p is q
+            if points_list.index(p) == points_list.index(q):
+                continue
+
+            # check if we have already calculated for these two points
+            exists = False
+            for d in distances:
+                if (d.get('p') or d.get('p') == 0) and (d.get('q') or d.get('q') == 0):
+                    if d['p'] == points_list.index(q) and d['q'] == points_list.index(p):
+                        exists = True
+                    if d['q'] == points_list.index(p) and d['p'] == points_list.index(q):
+                        exists = True
+
+            if exists:
+                continue
+
+            # get the distance
+            distance = get_distance(p,q)
+
+            # append to distances
+            distances.append({
+                'p': points_list.index(p),
+                'q': points_list.index(q),
+                'distance': distance
+            })
+
+    # now sort distances by distance
+    distances_sorted = sorted(distances, key=lambda d: d.get('distance'))
+
+    # get the respective points_list items for distances_sorted's first item (lowest distance)
+    return points_list[distances_sorted[0]['p']], points_list[distances_sorted[0]['q']]
+
+def generate_tuples_list_from_string(points_string):
+    """Take a string of points and return a list containing the tuples
+    """
+    # strip out the brackets and split by ','
+    # this should give us a list with individual items e.g. [1,2,3,4,...]
+    delta=points_string.replace('(', '').replace(')','').split(',')
+
+    # collect element pairs in the list into tuples
+    points_list = list(zip(delta[0::2], delta[1::2]))
+    return points_list
+
+def get_distance(p,q):
+    """Calculate the distance between points p and q.
+    p and q are validated tuples e.g. (942,42),(1.4,93)
+    """
+    # formula for calculating distance between p and q
+    distance = math.sqrt(square(float(q[0]) - float(p[0])) + square(float(q[1]) - float(p[1])))
+    return distance
+
+def square(x):
+    """Return the square of a number
+    """
+    return x*x
--- application/tests.py
+++ application/tests.py
@@ -1,3 +1,27 @@
+"""application tests
+"""
 from django.test import TestCase
+from .calculate import closest_pair, generate_tuples_list_from_string, square
 
-# Create your tests here.
+
+class ClosestPointsTestCase(TestCase):
+    """Test functions calculating the closest points pair
+    """
+    def test_closest_pair(self):
+        """Test closest pair function
+        """
+        points_string = '(45,12),(54.32,189.43),(94,19),(871,320)'
+        closest_points_pair = closest_pair(points_string)
+        self.assertEqual(closest_points_pair, (('45', '12'), ('94', '19')))
+
+    def test_generate_list_from_string(self):
+        """Test generate list from string function
+        """
+        points_string = '(45,12),(54.32,189.43),(94,19),(871,320)'
+        points_list = generate_tuples_list_from_string(points_string)
+        self.assertEqual(points_list, [('45', '12'), ('54.32', '189.43'), ('94', '19'), ('871', '320')])
+
+    def test_squares(self):
+        """Test squares function
+        """
+        self.assertEqual(square(2), 4)
